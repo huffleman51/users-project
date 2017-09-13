8d825a35ea20150d954c5cdc0075e5b688acb858
Dale Lepine <dalelepine@gmail.com>
2017-09-13 07:50:00 -0400

    Created User Unit Test

---
+++ test/create_test.js
@@ -0,0 +1,22 @@
+// const mongoose = require('mongoose');
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+const assert = require('assert');
+const User = require('../src/user');
+
+describe('Creating Records', () => {
+    it('saves a user', (done) => {
+        // Create a new Usesr object
+        const joe = new User({name: 'Joe'});
+        console.log(joe);
+
+        // Save User to database
+        joe.save()
+          .then(() => {
+            // Has Joe been saved to the database? Joe is not new, he has been saved
+              // Asset Test to make sure that User was saved to the database
+            assert(!joe.isNew);
+            done();
+          });
+    })
+});
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,9 +1,31 @@
 const mongoose = require('mongoose');
 // import mongoose from 'mongoose';
+const beforeEach = require('mocha').beforeEach;
+const before = require('mocha').before;
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-  .once('open', () => console.log('Good to go!'))
-  .on('error', (error) => {
-    console.warn('Error', error);
+mongoose.Promise = global.Promise;
+
+before((done) => {
+    mongoose.connection.openUri('mongodb://localhost/users_test');
+// mongoose.connect('mongodb://localhost/users_test', {useMongoClient: true});
+
+    mongoose.connection
+      .once('open', () => { })
+      .on('error', (error) => {
+          console.warn('Error', error);
+      });
+
+    //Drop User Collection before running any tests
+    mongoose.connection.collections.users.drop(() => {
+        // Ready to run the next test!
+        done();
+    });
 });
+
+// beforeEach((done) => {
+//     //Drop User Collection before running any tests
+//     mongoose.connection.collections.users.drop(() => {
+//         // Ready to run the next test!
+//         done();
+//     });
+// });
