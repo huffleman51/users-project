b68db692808f322f8ae4fdf2de68582c08e374fd
Dale Lepine <dalelepine@gmail.com>
2017-09-30 12:26:23 -0400

    Simple Update User model class level Unit Tests

--- test/update_test.js
+++ test/update_test.js
@@ -19,8 +19,9 @@ describe('Updating records', () => {
         assert(users.length === 1);
         assert(users[0].name === 'Alex');
         done();
-      })
+      });
   }
+
   it('instance type using set and save', (done) => {
     // console.log(joe);
 
@@ -32,10 +33,36 @@ describe('Updating records', () => {
     // Good for making incremental updates to an object and then saving it later
     assertName(joe.save(), done);
     // console.log(joe);
-  })
+  });
 
   it('A model instance can update', (done) => {
     // Good for bulk updates to an object and update immediately
     assertName(joe.update({name: 'Alex'}), done);
-  })
+  });
+
+  it('A model class can update', (done) => {
+    assertName(
+      User.update({name: 'Joe'}, {name: 'Alex'}),
+      done
+    );
+  });
+
+  it('A model class can update one record', (done) => {
+    assertName(
+      User.findOneAndUpdate({name: 'Joe'}, {name: 'Alex'}),
+      done
+    );
+  });
+
+  it('A model class can find a record with an Id and update', (done) => {
+    assertName(
+      User.findByIdAndUpdate(joe._id, {name: 'Alex'}),
+      done
+    );
+  });
+
+  it('', (done) => {
+
+    done();
+  });
 });
