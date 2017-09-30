417fa866d82e36dd7994d28ff19c480e27389d46
Dale Lepine <dalelepine@gmail.com>
2017-09-30 11:58:39 -0400

    Update User model instance level Unit Tests

--- test/update_test.js
+++ test/update_test.js
@@ -12,6 +12,15 @@ describe('Updating records', () => {
       .then(() => done());
   });
 
+  function assertName(operation, done) {
+    operation
+      .then(() => User.find({}))
+      .then((users) => {
+        assert(users.length === 1);
+        assert(users[0].name === 'Alex');
+        done();
+      })
+  }
   it('instance type using set and save', (done) => {
     // console.log(joe);
 
@@ -20,14 +29,13 @@ describe('Updating records', () => {
     joe.set('name', 'Alex');
 
     // To persist the name change to the database, you need to call dave
-    joe.save()
-      .then(() => User.find({}))
-      .then((users) => {
-        assert(users.length === 1);
-        assert(users[0].name === 'Alex');
-        done();
-      })
+    // Good for making incremental updates to an object and then saving it later
+    assertName(joe.save(), done);
     // console.log(joe);
   })
 
+  it('A model instance can update', (done) => {
+    // Good for bulk updates to an object and update immediately
+    assertName(joe.update({name: 'Alex'}), done);
+  })
 });
