eca17c49e90a926cb14657693470f39b27369137
Dale Lepine <dalelepine@gmail.com>
2017-09-30 12:58:40 -0400

    More Complex Update User model class level Unit Tests using mongo operators

--- test/update_test.js
+++ test/update_test.js
@@ -7,7 +7,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach((done) => {
-    joe = new User({name: 'Joe'});
+    joe = new User({name: 'Joe', postCount: 0});
     joe.save()
       .then(() => done());
   });
@@ -59,6 +59,21 @@ describe('Updating records', () => {
       User.findByIdAndUpdate(joe._id, {name: 'Alex'}),
       done
     );
+  });
+
+  it('A User can have their postCount incremented by one', (done) => {
+    // For every user named Joe, update the postCount to something
+    User.update({name: 'Joe'}, {$inc: {postCount: 1}})
+      .then(() => User.findOne({name: 'Joe'}))
+      .then((user) => {
+        assert(user.postCount === 1);
+        done();
+      });
+  });
+
+  it('', (done) => {
+
+    done();
   });
 
   it('', (done) => {
