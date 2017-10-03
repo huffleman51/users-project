aca1c5f473eee48d8bfd5c2128ad2b5ca8936618
Dale Lepine <dalelepine@gmail.com>
2017-10-02 20:41:18 -0400

    Testing adding a removing post sub documents to an exsiting user

--- test/subdocument_test.js
+++ test/subdocument_test.js
@@ -1,4 +1,5 @@
 import User from '../src/user';
+import Post from '../src/post'
 const chai = require('chai');
 const expect = require('chai').expect;
 const chaiAsPromised = require('chai-as-promised');
@@ -11,7 +12,8 @@ describe('Subdocuments', () => {
   it('can create a subdocument', async () => {
     const joe = new User({
       name: 'Joe',
-      posts: [{title: 'PostTitle'}]});
+      posts: [{title: 'PostTitle'}]}
+    );
 
     await joe.save()
       .then(() => User.findOne({name: 'Joe'}))
@@ -20,5 +22,42 @@ describe('Subdocuments', () => {
       });
   });
 
+  it('Can add sub documents to an existing record', async () => {
 
+    const joe = new User({
+      name: 'Joe',
+      posts: []
+    });
+
+    await joe.save()
+      .then(() => User.findOne({name: 'Joe'}))
+      .then((user) => {
+        user.posts.push({title: 'New Post'});
+        return user.save();
+      })
+      .then(() => User.findOne({name: 'Joe'}))
+      .then((user) => {
+        expect(user.posts[0].title).to.equal('New Post');
+      })
+  });
+
+  it('Can remove an existing sub document', async () => {
+    // const post = new Post({title: 'To Be Removed'});
+    const joe = new User({
+      name: 'Joe',
+      posts: [{title: 'To Be Removed'}]
+    });
+
+    await joe.save()
+      .then(() => User.findOne({name: 'Joe'}))
+      .then((user) => {
+        const post = user.posts[0];
+        user.posts.remove(post);
+        return user.save();
+      })
+      .then(() => User.findOne({name: 'Joe'}))
+      .then((user) => {
+        expect(user.posts.length).to.equal(0);
+      })
+  });
 });
