71ad491c100c660bf5e828ee64ffb2c58685b137
Dale Lepine <dalelepine@gmail.com>
2017-10-17 17:45:53 -0400

    Tested Sort, skip, limit

--- test/read_test.js
+++ test/read_test.js
@@ -7,12 +7,21 @@ chai.use(chaiAsPromised);
 
 describe('Reading users out of the database', () => {
 
-  let joe;
+  let joe, maria, alex, zach;
 
   beforeEach(async () => {
-    // Insert a User for the tests
+    // Insert a Users for the tests
+    alex = new User({name: 'Alex'});
     joe = new User({ name: 'Joe' } );
-    await joe.save();
+    maria = new User({name: 'Maria'});
+    zach = new User({name: 'Zach'});
+
+    // await alex.save();
+    // await joe.save();
+    // await maria.save();
+    // await zach.save();
+
+    await Promise.all([alex.save(), joe.save(), maria.save(), zach.save()]);
   });
 
   it('finds all users with a name of joe', async () => {
@@ -23,9 +32,22 @@ describe('Reading users out of the database', () => {
   });
 
   it('find a user with a particular id', async () => {
-    await User.findOne({ _id: joe._id  })
+    await User.findOne({_id: joe._id})
       .then((user) => {
         expect(user.name).to.equal('Joe');
       })
-  })
+  });
+
+  it('can sotr, skip, and limit the result set', async () => {
+    // -Alex- [Joe Maria] Zach
+    await User.find({})
+      .sort({name: 1})
+      .skip(1)
+      .limit(2)
+      .then((users) => {
+        expect(users.length).to.equal(2);
+        expect(users[0].name).to.equal('Joe');
+        expect(users[1].name).to.equal('Maria');
+      });
+  });
 });
